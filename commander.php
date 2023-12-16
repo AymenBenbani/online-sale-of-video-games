90edcc2301a126d53300b496c97f64b7f02a3e8b
aybenbani <148641530+AymenBenbani@users.noreply.github.com>
2023-12-16 15:04:26 +0100

    Add files via upload

---
+++ commander.php
@@ -0,0 +1,13 @@
+<?php
+//inclusion du fichier des fonctions php
+require_once('fonctions.php');
+// Connexion à MariaDB
+$db = connexion();
+
+/*-----------------------------------{script du bouton commander}------------------------------------------- */
+
+if (isset($_POST['article_id'])) // Verifier la récupération de article_id par le formulaire
+{
+    inserer_article($_POST['article_id'],$db); // appel de la fonction inserer_article  
+}
+?>
